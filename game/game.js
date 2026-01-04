0c0aa249b596c9a8af841c29ba6c0372ac87d772
Lee <126716094+Lepehn@users.noreply.github.com>
2026-01-03 23:52:40 -0600

    Update game.js

--- game/game.js
+++ game/game.js
@@ -2,9 +2,9 @@ import { GAME_CONFIG } from "./config.js";
 import { STATUSES, COMPLETED_STATUSES } from "./statuses.js";
 import { CATEGORIES } from "./categories.js";
 
-import { bosses } from "./data/bosses.js";
+import { boss } from "./data/boss.js";
 import { main } from "./data/main.js";
-import { weapons } from "./data/weapons.js";
+import { weapon } from "./data/weapon.js";
 
 console.log("GAME index loaded");
 
@@ -15,7 +15,7 @@ export const GAME = {
   categories: CATEGORIES,
   data: {
     main,
-    bosses,
-    weapons
+    boss,
+    weapon
   }
 };
